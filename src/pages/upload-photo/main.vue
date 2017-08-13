060a32af0da9859e4d0d022389c1706e3d04f886
houtan <houtan@qfpay.com>
2017-08-13 11:38:18 +0800

    upload

--- src/pages/upload-photo/main.vue
+++ src/pages/upload-photo/main.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="form-control">
-
+    <div class="space"></div>
+    <div class="container">
+      <img src="../../assets/shopHeader.png" alt="">
+      <div class="upload">Upload</div>
+      <img src="../../assets/shop_detail.png" alt="">
+      <div class="upload">Upload</div>
+    </div>
+    <div class="complete_btn">Complete</div>
   </div>
-
 </template>
   <script type="text/ecmascript-6">
   export default {
@@ -24,5 +30,47 @@
 
 <style scoped lang="scss" type="scss" rel="stylesheet/scss">
   @import "../../styles/common";
-
+  .form-control {
+    width: 100%;
+    position: relative;
+  }
+  .space {
+    width: 100%;
+    height: 140px;
+    background: #efefef;
+  }
+  img {
+    width: 100%;
+    height: 460px;
+  }
+  .container {
+    width: 100%;
+    padding: 0 40px;
+    margin: 100px 0;
+    div:first-of-type {
+      margin-bottom: 100px;
+    }
+  }
+ .upload {
+   width: 100%;
+   height: 80px;
+   border: 2px solid #FE9B20;
+   font-size: 32px;
+   color: #FE9B20;
+   line-height: 80px;
+   text-align: center;
+   border-radius: 6px;
+   margin: 20px 0;
+ }
+ .complete_btn {
+   width: 100%;
+   height: 80px;
+   background: #FE9B20;
+   text-align: center;
+   color: #fff;
+   font-size: 30px;
+   line-height: 80px;
+   position: fixed;
+   bottom: 0;
+ }
 </style>
